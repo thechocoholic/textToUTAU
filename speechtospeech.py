cf2b3a5e851b7adea5046e054f28d4443cc2e1e0
thechocoholic <idealchocolate@proton.me>
2026-09-19 21:49:34 +0700

    Create speechtospeech.py

---
+++ speechtospeech.py
@@ -0,0 +1,11 @@
+from RealtimeSTT import AudioToTextRecorder
+import pyttsx3
+# import threading 
+from pygame import mixer
+import os
+import time
+# import random
+# import string
+
+mixer.init(devicename="CABLE Input (VB-Audio Virtual Cable)")
+
