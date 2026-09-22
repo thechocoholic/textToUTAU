67b4ae5dc748bf85de6a8aae26677624a77e013d
thechocoholic <idealchocolate@proton.me>
2026-09-22 22:56:14 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,70 @@
+# this shit is so dead cause i dont know how openutau works adsfdsaydguk
+
+import re
+import tkinter as tk
+
+root=tk.Tk()
+
+# setting the windows size
+root.geometry("600x400")
+ 
+# declaring string variable
+
+sentence_var=tk.StringVar()
+
+ 
+# i dont understand shit
+
+def hell(text):
+    processing_text = text
+
+    processing_text = processing_text.replace("Java", "Python")
+
+    fixes = {
+        "im": "I'm",
+        "cant": "can't",
+        "dont": "don't",
+        "werent": "weren't",
+        "havent": "haven't",
+        "french bread": "baguette", # mandatory in true teto fashion
+    }
+
+    for old, new in fixes.items():
+        processing_text = re.sub(
+            r"\b" + old + r"\b",
+            new,
+            processing_text,
+            flags=re.IGNORECASE
+        )
+
+    return processing_text
+
+def submit():
+    print("inputted: " + sentence_var.get())
+    print("post processing input: " + hell(sentence_var.get()))
+    sentence_var.set("")   
+    
+    
+sentence_lable = tk.Label(root, text = 'sentence', font = ('calibre',10,'bold'))
+ 
+# creating a entry for sentence
+sentence_entry=tk.Entry(root, textvariable = sentence_var, font = ('calibre',10,'normal'))
+ 
+# creating a button using the widget 
+# button that will call the submit function 
+sub_btn=tk.Button(root,text = 'Submit', command = submit)
+ 
+# placing the label and entry in
+# the required position using grid
+# method
+sentence_lable.grid(row=0,column=0)
+sentence_entry.grid(row=0,column=1)
+sub_btn.grid(row=2,column=1)
+
+# honestly dont understands shit but this apparently
+# makes it work with the enter key so im happy
+root.bind("<Return>", lambda event: submit())
+
+# performing an infinite loop 
+# for the window to display
+root.mainloop()
