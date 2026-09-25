b60e9e20988cfe4daf2c56fa9c78f18ef0251f3b
thechocoholic <idealchocolate@proton.me>
2026-09-25 18:34:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-# this shit is so dead cause i dont know how openutau works adsfdsaydguk
-
 import re
 import tkinter as tk
 
 root=tk.Tk()
 
-# setting the windows size
-root.geometry("600x400")
- 
-# declaring string variable
+# self explanatory
 
+root.geometry("600x400")
 sentence_var=tk.StringVar()
 
  
 # i dont understand shit
-
-def hell(text):
+def text_correction_function_because_people_are_really_fucking_lazy(text):
     processing_text = text
 
     processing_text = processing_text.replace("Java", "Python")
@@ -42,46 +37,35 @@ def hell(text):
         "wont": "won't",
         "wouldnt": "wouldn't",
             
-            # this is mandatory trust.
+            # this is mandatory trust me.
         "french bread": "baguette", 
     }
+
 
     for old, new in fixes.items():
         processing_text = re.sub(
             r"\b" + old + r"\b",
             new,
             processing_text,
-            flags=re.IGNORECASE
+            flags = re.IGNORECASE
         )
 
     return processing_text
 
 def submit():
     print("inputted: " + sentence_var.get())
-    print("post processing input: " + hell(sentence_var.get()))
+    print("post processing input: " + text_correction_function_because_people_are_really_fucking_lazy(sentence_var.get()))
     sentence_var.set("")   
     
     
-sentence_lable = tk.Label(root, text = 'sentence', font = ('calibre',10,'bold'))
- 
-# creating a entry for sentence
+sentence_label = tk.Label(root, text = 'sentence, acronyms unsupported due to the nature of the auto correct', font = ('calibre',10,'bold'))
 sentence_entry=tk.Entry(root, textvariable = sentence_var, font = ('calibre',10,'normal'))
- 
-# creating a button using the widget 
-# button that will call the submit function 
 sub_btn=tk.Button(root,text = 'Submit', command = submit)
- 
-# placing the label and entry in
-# the required position using grid
-# method
-sentence_lable.grid(row=0,column=0)
-sentence_entry.grid(row=0,column=1)
-sub_btn.grid(row=2,column=1)
+sentence_label.grid(row=0,column=0)
+sentence_entry.grid(row=1,column=0)
+sub_btn.grid(row=2,column=0)
 
 # honestly dont understands shit but this apparently
 # makes it work with the enter key so im happy
 root.bind("<Return>", lambda event: submit())
-
-# performing an infinite loop 
-# for the window to display
-root.mainloop()
+root.mainloop() # dont know what this means but without it the thing just dies
